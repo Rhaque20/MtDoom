3784088e0d2f203841b907e8c2dbcee93f6e98f6
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2022-10-29 13:34:06 -0400

    Update Demon script

--- Assets/Scripts/Demons.cs
+++ Assets/Scripts/Demons.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class Demons : MonoBehaviour
 {
-    int demon = 0;//
+    int demon = 0;
     enum Difficulty{Easy,Normal,Hard};
     Difficulty d = Difficulty.Easy;
+
+    public Difficulty difficulty
+    {
+        get{return d;}
+    }// Return current difficulty
+
+    public int CallDemon()// Return randomly called demon
+    {
+        demon = Random.Range(1,3);
+        return demon;
+    }
+
+    public void RaiseDifficulty()
+    {
+        switch(d)
+        {
+            case Difficulty.Easy:
+                d = Difficulty.Normal;
+                break;
+            default:
+                d = Difficulty.Hard;
+                break;
+        }
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
