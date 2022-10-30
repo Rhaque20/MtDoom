8c9854c2c08771e19fac48c7864de4191866e1f2
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2022-10-30 12:47:27 -0400

    Rename DemonEvent to BushEvent

--- Assets/BushEvent.cs
+++ Assets/BushEvent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DemonEvent : MonoBehaviour
+public class BushEvent : MonoBehaviour
 {
     Collider col;
 
